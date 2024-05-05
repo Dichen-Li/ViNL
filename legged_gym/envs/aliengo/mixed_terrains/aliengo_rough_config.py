c58ed56f8c11827fca9b4cf91509a63a7b2e2a9d
Dichen Li <lchanggle2022@gmail.com>
2024-05-04 20:31:50 -0700

    try to record the joint angles in playing aliengo rough.pt

--- legged_gym/envs/aliengo/mixed_terrains/aliengo_rough_config.py
+++ legged_gym/envs/aliengo/mixed_terrains/aliengo_rough_config.py
@@ -127,6 +127,6 @@ class AliengoRoughCfgPPO(LeggedRobotCfgPPO):
         # resume_path = "weights/ny_rough_aliengo_Sep06_11-46-01_RoughTerrainDMEnc_model_1500.pt"
         
         # resume_path = "rough.pt" # rough.pt is the trained rough terrain policy.  Keep this line if you want to eval this policy via play.py.  Comment this line if you wish to train a rough terrain policy from scratch
-        resume_path = "weights/rough.pt" # rough.pt is the trained rough terrain policy.  Keep this line if you want to eval this policy via play.py.  Comment this line if you wish to train a rough terrain policy from scratch
+        resume_path = "weights/4.18aliengo/rough.pt" # rough.pt is the trained rough terrain policy.  Keep this line if you want to eval this policy via play.py.  Comment this line if you wish to train a rough terrain policy from scratch
         max_iterations = 4500
 
