039af49137e3ac29eb4a35391e340c6a819fa4db
Dichen-Li <lchanggle2022@gmail.com>
2024-05-11 00:53:41 -0700

    success for 3 envs

--- legged_gym/envs/spiderpi/mixed_terrains/spiderpi_obs_config.py
+++ legged_gym/envs/spiderpi/mixed_terrains/spiderpi_obs_config.py
@@ -27,9 +27,9 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
+import numpy as np
 
 from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
-import numpy as np
 
 """
 changes from a1 to aliengo
@@ -40,17 +40,14 @@ changes from a1 to aliengo
 """
 
 
-class SpiderpiRoughCfg(LeggedRobotCfg):
+class AliengoObsCfg(LeggedRobotCfg):
     class env(LeggedRobotCfg.env):
         # num_envs = 4096
-        num_envs = 3  # was getting a seg fault#ldc#set to 1 to avoid nan problem
+        num_envs = 1024  # was getting a seg fault
         # num_envs = 100  # was getting a seg fault
-        # num_actions = 12#ldc#urdf
-        num_actions = 18
-        # num_observations = 235
-        num_observations = 253#ldc#aliengo.urdf->spiderpi.urdf:235->253#due to the increase of legs, from 12 to 18. And there are three variables related to this.#6*3=18#235+18=253
-        # num_proprio_obs = 48
-        num_proprio_obs = 66#ldc#aliengo.urdf->spiderpi.urdf:235->253#due to the increase of legs, from 12 to 18. And there are three variables related to this.#6*3=18#48+18=66
+        num_actions = 12
+        num_observations = 235
+        num_proprio_obs = 48
         camera_res = [1280, 720]
         camera_type = "d"  # rgb
         num_privileged_obs = None  # 187
@@ -60,44 +57,27 @@ class SpiderpiRoughCfg(LeggedRobotCfg):
         float_cam=False
 
     class terrain(LeggedRobotCfg.terrain):
+        # terrain_proportions = [0.1, 0.1, 0.2, 0.2, 0.2, 0.2]
+        terrain_proportions = [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+        border = 50
         mesh_type = "trimesh"
 
     class init_state(LeggedRobotCfg.init_state):
         pos = [0.0, 0.0, 0.38]  # x,y,z [m]
 
-        # default_joint_angles = {  # = target angles [rad] when action = 0.0#ldc#urdf
-        #     "FL_hip_joint": 0.1,  # [rad]
-        #     "RL_hip_joint": 0.1,  # [rad]
-        #     "FR_hip_joint": -0.1,  # [rad]
-        #     "RR_hip_joint": -0.1,  # [rad]
-        #     "FL_thigh_joint": 0.8,  # [rad]
-        #     "RL_thigh_joint": 1.0,  # [rad]
-        #     "FR_thigh_joint": 0.8,  # [rad]
-        #     "RR_thigh_joint": 1.0,  # [rad]
-        #     "FL_calf_joint": -1.5,  # [rad]
-        #     "RL_calf_joint": -1.5,  # [rad]
-        #     "FR_calf_joint": -1.5,  # [rad]
-        #     "RR_calf_joint": -1.5,  # [rad]
-        # }
         default_joint_angles = {  # = target angles [rad] when action = 0.0
-            "body_leg_0": 0,  # [rad]
-            "leg_0_1_2": 1,  # [rad]
-            "leg_0_2_3": 1,  # [rad]
-            "body_leg_1": 0,  # [rad]
-            "leg_1_1_2": 1,  # [rad]
-            "leg_1_2_3": 1,  # [rad]
-            "body_leg_2": 0,  # [rad]
-            "leg_2_1_2": 1,  # [rad]
-            "leg_2_2_3": 1,  # [rad]
-            "body_leg_3": 0,  # [rad]
-            "leg_3_1_2": 1,  # [rad]
-            "leg_3_2_3": 1,  # [rad]
-            "body_leg_4": 0,  # [rad]
-            "leg_4_1_2": 1,  # [rad]
-            "leg_4_2_3": 1,  # [rad]
-            "body_leg_5": 0,  # [rad]
-            "leg_5_1_2": 1,  # [rad]
-            "leg_5_2_3": 1,  # [rad]
+            "FL_hip_joint": 0.1,  # [rad]
+            "RL_hip_joint": 0.1,  # [rad]
+            "FR_hip_joint": -0.1,  # [rad]
+            "RR_hip_joint": -0.1,  # [rad]
+            "FL_thigh_joint": 0.8,  # [rad]
+            "RL_thigh_joint": 1.0,  # [rad]
+            "FR_thigh_joint": 0.8,  # [rad]
+            "RR_thigh_joint": 1.0,  # [rad]
+            "FL_calf_joint": -1.5,  # [rad]
+            "RL_calf_joint": -1.5,  # [rad]
+            "FR_calf_joint": -1.5,  # [rad]
+            "RR_calf_joint": -1.5,  # [rad]
         }
 
     class control(LeggedRobotCfg.control):
@@ -113,25 +93,10 @@ class SpiderpiRoughCfg(LeggedRobotCfg):
         decimation = 4
 
     class asset(LeggedRobotCfg.asset):
-        file = "{LEGGED_GYM_ROOT_DIR}/resources/robots/spiderpi/urdf/spiderpi.urdf"#ldc#urdf
-        # foot_name = "foot"
-        foot_name=['dummy_eef_0', 'dummy_eef_1', 'dummy_eef_2', 'dummy_eef_3', 'dummy_eef_4', 'dummy_eef_5']#ldc#name the feet to be referenced in legged_robot.py
-        # penalize_contacts_on = ["thigh", "calf"]
-        penalize_contacts_on=[#ldc
-            'leg_0_2',
-            'leg_0_3',
-            'leg_1_2',
-            'leg_1_3',
-            'leg_2_2',
-            'leg_2_3',
-            'leg_3_2',
-            'leg_3_3',
-            'leg_4_2',
-            'leg_4_3',
-            'leg_5_2',
-            'leg_5_3']
-        terminate_after_contacts_on=[]#ldc
-        # terminate_after_contacts_on = ["base", "trunk", "hip"]
+        file = "{LEGGED_GYM_ROOT_DIR}/resources/robots/aliengo/urdf/aliengo.urdf"
+        foot_name = "foot"
+        penalize_contacts_on = ["thigh", "calf"]
+        terminate_after_contacts_on = ["base", "trunk", "hip"]
         self_collisions = 1  # 1 to disable, 0 to enable...bitwise filter
 
     class domain_rand(LeggedRobotCfg.domain_rand):
@@ -144,7 +109,10 @@ class SpiderpiRoughCfg(LeggedRobotCfg):
         only_positive_rewards = True
 
         class scales(LeggedRobotCfg.rewards.scales):
-            pass
+            feet_step = -1.0
+            # feet_step = 0.0
+            feet_stumble = -1.0
+            # feet_stumble = 0.0
 
     class evals(LeggedRobotCfg.evals):
         feet_stumble = True
@@ -152,19 +120,30 @@ class SpiderpiRoughCfg(LeggedRobotCfg):
         crash_freq = True
         any_contacts = True
 
+    class commands(LeggedRobotCfg.commands):
+        class ranges:
+            lin_vel_x = [0.0, 1.0]  # min max [m/s]
+            lin_vel_y = [0.0, 0.0]  # min max [m/s]
+            ang_vel_yaw = [-1.0, 1.0]  # min max [rad/s]
+            heading = [-0.5, 0.00]
 
-class SpiderpiRoughCfgPPO(LeggedRobotCfgPPO):
+    class noise(LeggedRobotCfg.noise):
+        add_noise = False
+
+
+class AliengoObsCfgPPO(LeggedRobotCfgPPO):
     class obsSize(LeggedRobotCfgPPO.obsSize):
         encoder_hidden_dims = [128, 64, 32]
 
     class runner(LeggedRobotCfgPPO.runner):
+        run_name = "ObsEncDM"
         alg = "ppo"
-        run_name = "RoughTerrainDMEnc"#ldc#the name of certain configure training pt file under ViNL/logs/experiment_name.
-        experiment_name = "rough_spiderpi"#ldc#the directory name for certain configure training pt file in ViNL/logs, also known as the log_dir in TaskRegisty.make_alg_runner or OnPolicyRunner.log_dir
+        # run_name = ""
+        experiment_name = "obs_aliengo"
         load_run = -1
-        # resume_path = "weights/ny_rough_aliengo_Sep06_11-46-01_RoughTerrainDMEnc_model_1500.pt"
-        
-        # resume_path = "rough.pt" # rough.pt is the trained rough terrain policy.  Keep this line if you want to eval this policy via play.py.  Comment this line if you wish to train a rough terrain policy from scratch
-        resume_path = "weights/5.10spiderpi/rough.pt" # rough.pt is the trained rough terrain policy.  Keep this line if you want to eval this policy via play.py.  Comment this line if you wish to train a rough terrain policy from scratch
-        max_iterations = 4500
+        max_iterations = 6000  # number of policy updates
+        num_test_envs=1
 
+        resume = True
+        resume_path = "weights/rough.pt" # if you want to train
+        # resume_path = "weights/obs.pt" #if you want to eval
